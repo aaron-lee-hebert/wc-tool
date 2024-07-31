c8a081310db39f0c0d6f10f9a22d15c034de61b3
Aaron Hebert <ahebert@tandem.app>
2024-07-31 12:41:06 -0500

    Complete Step One. Configure assemblies for publish

--- WordCountTool/Program.cs
+++ WordCountTool/Program.cs
@@ -1,23 +1,24 @@
-﻿using System.Runtime.ExceptionServices;
-
-namespace WordCountTool;
+﻿namespace WordCountTool;
 public class Program {
     static void Main(string[] args) {
-        bool verbose = false;
+        if (args.Length < 2 || args.Length % 2 != 0) {
+            Console.WriteLine("Usage: ccwc -c <file-path>");
+            return;
+        }
 
         for (int i = 0; i < args.Length; i++) {
             switch (args[i]) {
+                // Output the number of bytes in the file
                 case "-c":
                     if (i + 1 < args.Length) {
                         string? filePath = args[i + 1];
                         i++;
-                        Console.WriteLine(FileProcessor.GetByteCount(filePath));
+                        Console.WriteLine($"\t{FileProcessor.GetByteCount(filePath)} {filePath}");
                     } else {
                         Console.WriteLine("Error: Missing value for -c option.");
                         return;
                     }
                     break;
-
                 default:
                     Console.WriteLine($"Unknown option: {args[i]}");
                     return;
