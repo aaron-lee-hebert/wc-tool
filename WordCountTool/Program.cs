bed87fb6f7cfec20983fd7ae9b607b15690f006b
Aaron Hebert <ahebert@tandem.app>
2024-08-02 17:11:15 -0500

    Complete Step Five. Consolidate file check into own method.

--- WordCountTool/Program.cs
+++ WordCountTool/Program.cs
@@ -1,8 +1,15 @@
 ﻿namespace WordCountTool;
 public class Program {
     static void Main(string[] args) {
-        if (args.Length < 2 || args.Length % 2 != 0) {
+        if (args.Length == 0) {
             PrintUsage();
+            return;
+        }
+
+        if (args.Length == 1) {
+            string filePath = args[0];
+            if (!CheckFileExists(filePath)) return;
+            Console.WriteLine($"\t{FileProcessor.GetByteCount(filePath)}  {FileProcessor.GetLineCount(filePath)}  {FileProcessor.GetWordCount(filePath)} {filePath}");
             return;
         }
 
@@ -10,10 +17,7 @@ public class Program {
             string option = args[i];
             string filePath = args[i + 1];
 
-            if (!File.Exists(filePath)) {
-                Console.WriteLine($"Error: File not found: {filePath}");
-                continue;
-            }
+            if (!CheckFileExists(filePath)) continue;
 
             switch (option) {
                 case "-c":
@@ -37,11 +41,21 @@ public class Program {
     }
 
     static void PrintUsage() {
-        Console.WriteLine("Usage: ccwc [-c <file-path>] [-l <file-path>] [-w <file-path>] [-m <file-path>]");
+        Console.WriteLine("Usage: ccwc [<file-path>] [-c <file-path>] [-l <file-path>] [-w <file-path>] [-m <file-path>]");
         Console.WriteLine("\nExamples:");
+        Console.WriteLine("  ccwc <file-path>     # Outputs the number of bytes, lines, and words in the file");
         Console.WriteLine("  ccwc -c <file-path>  # Outputs the number of bytes in the file");
         Console.WriteLine("  ccwc -l <file-path>  # Outputs the number of lines in the file");
         Console.WriteLine("  ccwc -w <file-path>  # Outputs the number of words in the file");
         Console.WriteLine("  ccwc -m <file-path>  # Outputs the number of chars in the file");
+    }
+
+    static bool CheckFileExists(string filePath) {
+        if (!File.Exists(filePath)) {
+            Console.WriteLine($"Error: File not found: {filePath}");
+            return false;
+        }
+
+        return true;
     }
 }
